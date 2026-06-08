f1144fbd6a3659752f0c3215c2d64e7465c3eed2
theakhilsarkar <akhilsodvadiya7@gmail.com>
2026-06-08 18:29:12 +0530

    ...

---
+++ modules/combine/server.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+
+// if (!fs.existsSync("data")) {
+//   fs.mkdirSync("data");
+// }
+
+// fs.writeFileSync("./data/notes.txt", "File System Module");
+// fs.writeFileSync("./data/notes2.txt", "File System Module");
+// fs.writeFileSync("./data/notes3.txt", "File System Module");
+// fs.writeFileSync("./data/notes4.txt", "File System Module");
+// fs.writeFileSync("./data/notes5.txt", "File System Module");
+// console.log(fs.readFileSync("./data/notes.txt", "utf-8"));
+
+// fs.unlinkSync("./data/notes.txt");
+// console.log(fs.readdirSync("data"));
+// fs.rmdirSync("data"); - will remove after empty whole folder.
